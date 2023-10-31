3eb44fddc99d3503cfaaec050a0130f1dea38962
jdonlan91 <joedonlan1991@gmail.com>
2023-10-31 16:57:40 +0000

    reformat test_dump_data for PEP-8 compliance

--- test/test_ingester/test_dump_data.py
+++ test/test_ingester/test_dump_data.py
@@ -1,5 +1,4 @@
 from moto import mock_s3
-from moto.core import patch_client
 import pytest
 import os
 import boto3
@@ -37,20 +36,6 @@ def test_csv():
         seq,name/first,name/last,age,street,city,state,zip,dollar,pick,date
         1,Curtis,Ruiz,54,Relgo Way,Zappeguk,MN,25633,$4978.53,WHITE,10/30/1941
         2,Jon,Haynes,54,Jozif Terrace,Zegzavid,ND,41182,$754.87,BLUE,06/12/2048
-        3,Ada,Carlson,61,Cicrer Boulevard,Utetude,ME,21912,$9240.52,YELLOW,11/18/1983
-        4,Lester,Reyes,43,Gehru Manor,Ketowar,DE,05912,$9398.47,WHITE,09/01/1955
-        5,Lawrence,Estrada,46,Dufcas Lane,Ocfevap,ME,15216,$4241.11,BLUE,05/14/2045
-        6,Theresa,Waters,29,Waleh Avenue,Hiwjidgi,MN,12857,$6287.90,GREEN,08/06/1944
-        7,Willie,Ferguson,36,Cignud Center,Lemenodu,DC,26480,$8424.98,BLUE,02/02/1980
-        8,Thomas,Rodriquez,36,Mesa Boulevard,Punfizcul,CO,94689,$2930.15,WHITE,02/05/1930
-        9,Caleb,Bradley,62,Sepuw Extension,Objojal,NY,70755,$8556.95,GREEN,12/28/2051
-        10,Miguel,Green,37,Polca Key,Ramuka,MS,94261,$7878.88,BLUE,12/05/1954
-        11,Josie,Vaughn,38,Zugo Terrace,Migejut,WV,74799,$2634.21,WHITE,11/22/1943
-        12,Connor,White,25,Ejumuw Pike,Bucagsu,DE,26768,$848.69,YELLOW,02/19/1959
-        13,Brian,Newton,18,Nonbep Plaza,Kufloaf,TX,53478,$1895.66,GREEN,04/08/2022
-        14,Mina,Higgins,20,Daive Mill,Sepaplif,IA,24221,$6935.14,YELLOW,09/14/1941
-        15,Essie,Leonard,39,Cokez Point,Suriweko,WY,94170,$3768.29,GREEN,11/28/1946
-        16,Allie,Lowe,29,Nehrik Avenue,Modwiwuj,MN,78389,$6009.16,RED,01/23/2034
     """
 
 
@@ -63,12 +48,6 @@ class TestDumpData:
 
     def test_puts_csv_file_in_bucket(self, s3, test_csv, empty_bucket):
         dump_data("test_table", "31-10-23-152600", test_csv, "test_ingested_bucket")
-        response = s3.list_objects(
-            Bucket="test_ingested_bucket",
-        )
-        assert response["Contents"][0]["Key"] == "test_table-31-10-23-152600.csv"
-
-
-# dumps CSV data into ingestion bucket
-
-# throws an error if passed a filetype that is not CSV
+        response = s3.list_objects(Bucket="test_ingested_bucket")
+        filename = response["Contents"][0]["Key"]
+        assert filename == "test_table-31-10-23-152600.csv"
