e8e726da0063639661dd889a464798d8e9acc314
jdonlan91 <joedonlan1991@gmail.com>
2023-10-31 16:56:26 +0000

    add docstring to dump_data

--- src/ingester/dump_data.py
+++ src/ingester/dump_data.py
@@ -1,27 +1,35 @@
-# Arguments: table_name, string of data in CSV format
-
-# Returns: a string indicating success, possibly containing metadata about the file that has been dumped ad the bucket it has been dumped to.
-
-# Dumps CSV data into ingestion bucket.
-
-# s3://ingestion/table-name-timestamp.csv
-
 import boto3
 
-client = boto3.client("s3")
 
-
-def genereate_object_key(table_name, timestamp):
+def generate_object_key(table_name, timestamp):
     return f"{table_name}-{timestamp}.csv"
 
 
 def dump_data(table_name, timestamp, csv_data, bucket_name):
+    """Dumps csv-formatted data into S3 bucket as .csv file
+
+    File will be named '{table_name}-{timestamp}.csv'.
+    Intended to be called by a parent AWS Lambda function.
+
+    Args:
+        table_name: name of table from which data was extracted.
+        timestamp: timestamp when parent Lambda was called.
+        csv_data: string of data in csv format.
+        bucket_name: name of destination S3 bucket.
+
+    Returns:
+        The string "Data dumped successfully!"
+
+    Raises:
+        ClientError: an error occurred in accessing
+        (or writing the file to) the named S3 bucket.
+
     """
-    DOCSTRING GOES HERE
-    """
-    object_key = genereate_object_key(table_name, timestamp)
+    object_key = generate_object_key(table_name, timestamp)
 
     try:
+        client = boto3.client("s3")
+
         response = client.put_object(
             Body=csv_data,
             Bucket=bucket_name,
@@ -30,15 +38,14 @@ def dump_data(table_name, timestamp, csv_data, bucket_name):
 
         response_metadata = response["ResponseMetadata"]
 
-        response_string = f"""
+        print(
+            f"""
         HTTP status code: {response_metadata['HTTPStatusCode']}
         Timestamp: {response_metadata['HTTPHeaders']['last-modified']}
         """
+        )
 
-        if response_metadata["HTTPStatusCode"] == 200:
-            print(response_string)
-            return "Data dumped successfully!"
+        return "Data dumped successfully!"
 
-        return response
     except Exception as e:
-        print(e)
+        raise e
