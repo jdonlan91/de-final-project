b287c741ad9048b2990632e1820595ae4054499e
Chris <chris79gp@outlook.com>
2023-11-02 09:35:24 +0000

    revise docstring to be more descriptive remove comments from head of file

--- src/ingestor/utils/fetch_new_data.py
+++ src/ingestor/utils/fetch_new_data.py
@@ -1,38 +1,25 @@
 from pg8000.native import Connection, identifier, literal
 from datetime import datetime
 
-# To allow us to identify new data that has appeared
-# since the last invocation of this function on a particular table,
-# we can use use a WHERE clause for the last_updated column (if it exists)
-
-# Depending on the how often this function is called (e.g. every 1 min)
-# we can use the current timestamp, subtracting that interval in the clause.
-# ! If we do this, we'd probably need a second parameter for the interval !
-
-# The ERD shows that all the production tables have a last_updated column
-
 
 def fetch_new_data(table_name: str, timestamp, db_credentials) -> list[dict]:
     """Fetches data more recent than the timestamp from a specified table
     and returns the rows as a list of dictionaries
 
-        Args:
-                table_name <string>: The table name
-                timestamp <datetime object>: timestamp after
-                which data will be selected
-                db_credentials <dictionary>: credentials
-                for accessing the database with format.
-                It needs to have the following format:
-                    {
-                       "DB_USERNAME": ...
-                        "DB_NAME": ...
-                        "DB_HOST": ...
-                        "DB_PASSWORD": ...
-                    }
+    Args:
+        table_name <string>: The table name
+        timestamp <datetime object>: timestamp from which data will be selected
+        db_credentials <dictionary>: credentials for accessing the database.
+        Credentials needs to be in the following format:
+            {
+                "DB_USERNAME": ...
+                "DB_NAME": ...
+                "DB_HOST": ...
+                "DB_PASSWORD": ...
+            }
 
-        Returns:
-                returns <list[dict]> representing the relevant
-                rows of the database
+    Returns:
+        returns <list[dict]> representing the relevant rows of the database
     """
     try:
         conn = Connection(
