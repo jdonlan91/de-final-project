a86f4d3483d19318d8c5107853edf7dfa8716969
santhyt <san7859@hotmail.com>
2023-11-03 12:01:51 +0000

    add testing suite for fetch_table_names

--- src/ingestor/utils/fetch_table_names.py
+++ src/ingestor/utils/fetch_table_names.py
@@ -1,4 +1,13 @@
 def fetch_table_names():
     """Fetches table names from database for lambda_handler function
+    
+    Args:
+        database_name <string> the name of the database to fetch the table names
+    
+    Returns:
+        <list> a list of table names in the database
+    
+    Reads and fetches all the table names from a given database, 
+    returning them in a list.
     """
     pass
--- test/test_ingestor/test_fetch_table_names.py
+++ test/test_ingestor/test_fetch_table_names.py
@@ -1,1 +1,22 @@
+import pytest
+
 from src.ingestor.utils.fetch_table_names import fetch_table_names
+
+class TestFetchTableNames:
+    def test_returns_a_list(self):
+        result = fetch_table_names('test_database')
+        assert isinstance(result, list)
+        
+    def test_returns_an_empty_list(self):
+        result = fetch_table_names('empty_database')
+        assert result == []
+        
+    def test_returns_a_list_of_table_names(self):
+        result = fetch_table_names('test_database')
+        expected = ['table_one', 'table_two', 'table_three']
+        assert result == expected
+        
+    def test_raises_an_exception(self):
+        result = fetch_table_names('invalid_database')
+        with pytest.raises(Exception):
+            result
