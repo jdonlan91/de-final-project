986d3354cf39467c0090efe0d96e053a44f8bd78
santhyt <san7859@hotmail.com>
2023-11-03 11:33:41 +0000

    initial commit for fetch new table names

---
+++ src/ingestor/utils/fetch_table_names.py
@@ -0,0 +1,4 @@
+def fetch_table_names():
+    """Fetches table names from database for lambda_handler function
+    """
+    pass
---
+++ test/test_ingestor/test_fetch_table_names.py
@@ -0,0 +1,1 @@
+from src.ingestor.utils.fetch_table_names import fetch_table_names
