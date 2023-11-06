e5d0130821f54369343a7e5903aa5cabfc737651
IuliaKostina <iuliiakostina@icloud.com>
2023-11-06 13:56:07 +0000

    add util path variable to the Makefile change import paths in ingestor change the layer references to relative paths in lambda

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "ingestor" {
     function_name = "ingestor"
-    filename = "${path.module}/../src/ingestor/ingestor_payload.zip"
-    layers = [aws_lambda_layer_version.ingestor_utils_layer.arn]
+    filename = data.archive_file.ingestor_lambda.output_path
+    layers = [aws_lambda_layer_version.ingestor_utils_layer.arn, "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python311:2"]
     role = aws_iam_role.ingestor_lambda_role.arn
     handler = "ingestor.lambda_handler"
     source_code_hash = data.archive_file.ingestor_lambda.output_base64sha256
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "ingestor" {
 
 
 resource "aws_lambda_layer_version" "ingestor_utils_layer" {
-    filename = "${path.module}/../src/ingestor/utils_payload.zip"
+    filename = data.archive_file.ingestor_utils.output_path
     layer_name = "ingestor_utils_layer"
     source_code_hash = data.archive_file.ingestor_utils.output_base64sha256
 }
