42be60734db5e5660879b209e53004888568116c
santhyt <san7859@hotmail.com>
2023-11-06 10:45:45 +0000

    update generate_object_key to return a file path in the format table_name/date/timestamp.csv

--- test/test_ingestor/test_dump_data.py
+++ test/test_ingestor/test_dump_data.py
@@ -4,11 +4,17 @@ import pytest
 import boto3
 from moto import mock_s3
 
-from src.ingestor.utils.dump_data import dump_data
+from src.ingestor.utils.dump_data import (
+    generate_object_key,
+    dump_data
+)
 
+class TestGenerateObjectKey:
+    def test_generate_object_key(self):
+       expected = "test_table/31-10-23/31-10-23-152600.csv"
+       result = generate_object_key("test_table", "31-10-23-152600")
 
-class TestDumpData:
-    
+class TestDumpData:    
     @pytest.fixture(scope="function")
     def aws_credentials(self):
         """Mocked AWS Credentials for moto."""
@@ -19,12 +25,10 @@ class TestDumpData:
         os.environ["AWS_SESSION_TOKEN"] = "test"
         os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
-
     @pytest.fixture(scope="function")
     def s3(self, aws_credentials):
         with mock_s3():
             yield boto3.client("s3", region_name="eu-west-2")
-
 
     @pytest.fixture
     def empty_bucket(self, s3):
@@ -48,7 +52,7 @@ class TestDumpData:
                 "31-10-23-152600",
                 test_csv,
                 "test_ingested_bucket"
-            ) == "test_table-31-10-23-152600.csv"
+            ) == "test_table/31-10-23/31-10-23-152600.csv"
         )
 
     def test_puts_csv_file_in_bucket(self, s3, test_csv, empty_bucket):
@@ -60,7 +64,7 @@ class TestDumpData:
         )
         response = s3.list_objects(Bucket="test_ingested_bucket")
         filename = response["Contents"][0]["Key"]
-        assert filename == "test_table-31-10-23-152600.csv"
+        assert filename == "test_table/31-10-23/31-10-23-152600.csv"
 
     def test_raises_an_error(self, test_csv):
         with pytest.raises(Exception):
