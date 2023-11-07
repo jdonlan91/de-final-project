f77d0e9ccf89b119d1c6712903d3bccc22fb89a7
jdonlan91 <joedonlan1991@gmail.com>
2023-11-07 16:22:29 +0000

    add docstring to read_csv function

--- src/processor/utils/read_csv.py
+++ src/processor/utils/read_csv.py
@@ -2,6 +2,17 @@ import boto3
 
 
 def read_csv(file_name, bucket_name):
+    """Reads the contents of a csv file in an AWS S3 bucket.
+
+    Args:
+        file_name <string> the name of the file. 
+        bucket_name <string> the name of the S3 bucket
+        containing the file.
+
+    Returns:
+        <list> the contents of the csv file (each row is a dictionary
+        with the keys as the column names).
+    """
     s3 = boto3.client("s3")
     response = s3.get_object(
         Bucket=bucket_name,
