3f96d6c069f521eaa269f2ac2da5ef8d3d4e5bfb
jdonlan91 <joedonlan1991@gmail.com>
2023-11-07 16:02:19 +0000

    refactor fixture for create_test_csv_files add test for returning empty list when csv fle contains no data

--- test/test_processor/test_read_csv.py
+++ test/test_processor/test_read_csv.py
@@ -33,18 +33,27 @@ class TestReadCsv:
         )
 
     @pytest.fixture(autouse=True)
-    def create_test_csv_file(self, empty_bucket):
-        csv_string = """id,firstname,lastname
+    def create_test_csv_files(self, empty_bucket, s3):
+        test_csv_data = """id,firstname,lastname
                         1,Rosanne,Harriman
                         2,Hermione,Leler
                         3,Bernardine,Cosenza"""
 
-        dump_data("test_table", datetime.strptime("31-10-2023-152600",
-                                                  "%d-%m-%Y-%H%M%S"), csv_string, "test_ingested_bucket")
+        s3.put_object(
+            Body=test_csv_data,
+            Bucket="test_ingested_bucket",
+            Key="test_folder/test_file.csv"
+        )
 
-    def test_returns_list_of_dictionaries(self, create_test_csv_file):
+        s3.put_object(
+            Body="",
+            Bucket="test_ingested_bucket",
+            Key="test_folder/test_empty_file.csv"
+        )
+
+    def test_returns_list_of_dictionaries(self):
         result = read_csv(
-            "test_table/31-10-2023/31-10-2023-152600.csv",
+            "test_folder/test_file.csv",
             "test_ingested_bucket")
         assert isinstance(result, list)
 
@@ -52,7 +61,10 @@ class TestReadCsv:
             assert isinstance(item, dict)
 
     def test_returns_empty_list_if_passed_file_with_no_data(self):
-        pass
+        result = read_csv(
+            "test_folder/test_empty_file.csv",
+            "test_ingested_bucket")
+        assert result == []
 
     def test_returns_contents_of_csv_as_list_of_dictionaries(self):
         pass
