e4b19fbaf2d152b7ad1ff033c185ea0633be802d
IuliaKostina <iuliiakostina@icloud.com>
2023-11-09 17:01:12 +0000

    add docstring to populate_schema

---
+++ src/loader/utils/populate_schema.py
@@ -0,0 +1,15 @@
+def populate_schema(csv_string):
+    """
+    Takes data in csv format from read_parquet and populates
+    a star schema database with this data.
+
+    Args:
+        csv string: string from read_parquet.
+
+    Returns:
+
+    Raises:
+        Exception when error connecting to database.
+
+    """
+    pass
