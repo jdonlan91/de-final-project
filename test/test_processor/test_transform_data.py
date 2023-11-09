ef8509f3d48344a0c9eb901c983d77e35a970670
Chris <chris79gp@outlook.com>
2023-11-09 10:51:41 +0000

    add unit tests for get_db_credentials and create_conection

--- test/test_processor/test_transform_data.py
+++ test/test_processor/test_transform_data.py
@@ -1,10 +1,71 @@
+import json
 import pytest
 
+import boto3
+from moto import mock_secretsmanager
+from pg8000.exceptions import InterfaceError
 from unittest.mock import patch
 
-from src.processor.utils.transform_data import *
-from fixtures.transform_staff_data import test_staff_data
-from fixtures.transform_sales_data import test_sales_data
+from src.processor.utils.transform_data import (
+    get_db_credentials,
+    create_connection,
+    # query_database,
+    transform_staff,
+    transform_sales_order,
+    # transform_data
+)
+
+from fixtures.transform_staff_data import test_staff_data  # noqa: F401
+from fixtures.transform_sales_data import test_sales_data  # noqa: F401
+
+
+class TestCreateConnection:
+    def test_create_connection_failure(self):
+        db_credentials = {
+            "DB_USERNAME": "invalid_user",
+            "DB_PASSWORD": "invalid_password",
+            "DB_HOST": "invalid_host",
+            "DB_NAME": "invalid_db",
+        }
+
+        with pytest.raises(InterfaceError):
+            create_connection(db_credentials)
+
+
+class TestGetDbCredentials:
+    @mock_secretsmanager
+    def test_get_db_credentials_returns_correct_credentials(self):
+        conn = boto3.client("secretsmanager", region_name="eu-west-2")
+        conn.create_secret(
+            Name="totesys_db_credentials",
+            SecretString=json.dumps(
+                {
+                    "host": "test-host",
+                    "password": "test_password",
+                    "username": "test_username",
+                    "dbname": "test_db_name",
+                }
+            ),
+        )
+        db_credentials = get_db_credentials()
+        assert db_credentials["DB_HOST"] == "test-host"
+
+    @mock_secretsmanager
+    def test_get_db_credentials_raises_error_when_fails_to_access_secret(self):
+        conn = boto3.client("secretsmanager", region_name="eu-west-2")
+        conn.create_secret(
+            Name="wrong_db_credentials",
+            SecretString=json.dumps(
+                {
+                    "host": "test-host",
+                    "password": "test_password",
+                    "username": "test_username",
+                    "dbname": "test_db_name",
+                }
+            ),
+        )
+        with pytest.raises(Exception):
+            get_db_credentials()
 
 
 class TestTransformStaff():
@@ -19,10 +80,7 @@ class TestTransformStaff():
 
             yield mock_query_database
 
-    def test_returns_list_of_dictionaries(
-        self,
-        test_staff_data
-    ):
+    def test_returns_list_of_dictionaries(self, test_staff_data):  # noqa: F811
         test_input_staff_data, test_output_staff_data = test_staff_data
 
         result = transform_staff(test_input_staff_data)
@@ -35,10 +93,7 @@ class TestTransformStaff():
     def test_returns_empty_list_if_passed_file_with_no_data(self):
         assert transform_staff([]) == []
 
-    def test_returns_contents_of_csv_as_list_of_dictionaries(
-        self,
-        test_staff_data
-    ):
+    def test_returns_transformed_data(self, test_staff_data):  # noqa: F811
         test_input_staff_data, test_output_staff_data = test_staff_data
 
         result = transform_staff(test_input_staff_data)
@@ -48,7 +103,7 @@ class TestTransformStaff():
 
 
 class TestTransformSalesOrder():
-    def test_returns_list_of_dictionaries(self, test_sales_data):
+    def test_returns_list_of_dictionaries(self, test_sales_data):  # noqa: F811
         test_input_sales_data, test_output_sales_data = test_sales_data
 
         result = transform_sales_order(test_input_sales_data)
@@ -58,13 +113,13 @@ class TestTransformSalesOrder():
         for item in result:
             assert isinstance(item, dict)
 
-    def test_returns_empty_list_if_passed_file_with_no_data(self, test_sales_data):
+    def test_returns_empty_list_if_passed_file_with_no_data(
+        self,
+        test_sales_data  # noqa: F811
+    ):
         assert transform_sales_order([]) == []
 
-    def test_returns_contents_of_csv_as_list_of_dictionaries(
-        self,
-        test_sales_data
-    ):
+    def test_returns_transformed_data(self, test_sales_data):  # noqa: F811
         test_input_sales_data, test_output_sales_data = test_sales_data
 
         result = transform_sales_order(test_input_sales_data)
