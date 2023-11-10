5f09ed6b5c5e5c8aa4d6b31cd1a3a68c4e63ea4c
Chris <chris79gp@outlook.com>
2023-11-10 12:13:16 +0000

    add docstring for read_parquet

--- src/loader/utils/read_parquet.py
+++ src/loader/utils/read_parquet.py
@@ -1,2 +1,14 @@
+import pyarrow.parquet as pq
+
+
 def read_parquet(file_name, bucket_name):
+    """Reads the contents of a parquet file in an AWS S3 bucket.
+
+    Args:
+        file_name <string> the name of the parquet file.
+        bucket_name <string> the name of the S3 bucket containing the file.
+
+    Returns:
+        <str> the contents of the parquet file in csv format.
+    """
     pass
