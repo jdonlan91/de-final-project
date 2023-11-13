6c9422275f8003cc090140d607ae86dadf04645a
IuliaKostina <iuliiakostina@icloud.com>
2023-11-13 12:20:00 +0000

    add populate_schema utility function for loader add tests for populate_schema

--- src/loader/utils/populate_schema.py
+++ src/loader/utils/populate_schema.py
@@ -1,4 +1,17 @@
-def populate_schema(csv_string):
+from pg8000.native import Connection, identifier
+from io import StringIO
+
+
+def create_connection(db_credentials):
+    return Connection(
+        user=db_credentials["DB_USERNAME"],
+        password=db_credentials["DB_PASSWORD"],
+        host=db_credentials["DB_HOST"],
+        database=db_credentials["DB_NAME"],
+    )
+
+
+def populate_schema(db_credentials, table_name, csv_string):
     """
     Takes data in csv format from read_parquet and populates
     a star schema database with this data.
@@ -12,4 +25,37 @@ def populate_schema(csv_string):
         Exception when error connecting to database.
 
     """
-    pass
+    try:
+        conn = create_connection(db_credentials)
+
+        temp_tbl_name = f"temp_{table_name}"
+        column_names = csv_string.split("\n")[0]
+        column_names_array = column_names.split(",")
+
+        create_temp_tbl = f"""CREATE TEMP TABLE {identifier(temp_tbl_name)}
+ (LIKE {table_name}) ON COMMIT DROP;"""
+
+        populate_temp = f"""COPY {identifier(temp_tbl_name)} ({column_names})
+ FROM STDIN WITH CSV HEADER;"""
+
+        query = f"""INSERT INTO {identifier(table_name)} ({column_names})
+ SELECT * FROM {identifier(temp_tbl_name)}
+ ON CONFLICT ({identifier(column_names_array[0])}) DO UPDATE SET"""
+
+        for column in column_names_array:
+            logic = f" {identifier(column)} = EXCLUDED.{identifier(column)},"
+            if column != column_names_array[0]:
+                query += logic
+
+        formatted_query = f"{query[:-1]};"
+
+        csv_obj = StringIO(csv_string)
+
+        conn.run("BEGIN;")
+        conn.run(create_temp_tbl)
+        conn.run(populate_temp, stream=csv_obj)
+        conn.run(formatted_query)
+        conn.run("COMMIT;")
+
+    except Exception as e:
+        raise e
